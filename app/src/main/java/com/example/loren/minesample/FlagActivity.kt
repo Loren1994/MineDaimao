74bc23e38c863955ee210b21cf282428ff446066
Loren <dayan805@163.com>
2016-09-19 19:24:09 +0800

    modify china flag

---
+++ app/src/main/java/com/example/loren/minesample/FlagActivity.kt
@@ -0,0 +1,37 @@
+package com.example.loren.minesample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ *                            _ooOoo_
+ *                           o8888888o
+ *                           88" . "88
+ *                           (| -_- |)
+ *                            O\ = /O
+ *                        ____/`---'\____
+ *                      .   ' \\| |// `.
+ *                       / \\||| : |||// \
+ *                     / _||||| -:- |||||- \
+ *                       | | \\\ - /// | |
+ *                     | \_| ''\---/'' | |
+ *                      \ .-\__ `-` ___/-. /
+ *                   ___`. .' /--.--\ `. . __
+ *                ."" '< `.___\_<|>_/___.' >'"".
+ *               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ *                 \ \ `-. \_ __\ /__ _/ .-` / /
+ *         ======`-.____`-.___\_____/___.-`____.-'======
+ *                            `=---='
+ *
+ *         .............................................
+ *                  佛祖保佑             永无BUG
+ *
+ *                Copyright (c) 16-9-19 by loren
+ */
+class FlagActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.flag_activity)
+    }
+}
--- app/src/main/java/com/example/loren/minesample/LauncherActivity.java
+++ app/src/main/java/com/example/loren/minesample/LauncherActivity.java
@@ -13,11 +13,17 @@ public class LauncherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.java_kotlin_activity);
+        setContentView(R.layout.launcher_activity);
         findViewById(R.id.fs_tv).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LauncherActivity.this, CustomViewActivity.class));
+            }
+        });
+        findViewById(R.id.flag_tv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LauncherActivity.this, FlagActivity.class));
             }
         });
     }
