1fe75dd4677c3508a8e5a49dfa4e5b0f25ada8c9
Loren <dayan805@163.com>
2017-02-15 13:43:17 +0800

    add exit anim

--- app/src/main/java/com/example/loren/minesample/OpenScreenActivity.java
+++ app/src/main/java/com/example/loren/minesample/OpenScreenActivity.java
@@ -35,6 +35,7 @@ public class OpenScreenActivity extends Activity implements OpenScreen.onDialogL
     private long TOTAL_TIME = 10000;
     private int errorNum = 0;
     private Timer timer;
+    private Animator endAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +46,7 @@ public class OpenScreenActivity extends Activity implements OpenScreen.onDialogL
         openOs.setPassword(password);
         animator = ObjectAnimator.ofFloat(victorIv, "alpha", 0f, 1f).setDuration(1000);
         timer = new Timer(TOTAL_TIME, 1000);
+
         startAnim();
     }
 
@@ -79,7 +81,7 @@ public class OpenScreenActivity extends Activity implements OpenScreen.onDialogL
         parentRl.post(new Runnable() {
             @Override
             public void run() {
-                // 圆形动画的x,y坐标  位于View的中心
+                //圆形动画的x,y坐标  位于View的中心
                 int cx = (parentRl.getLeft() + parentRl.getRight()) / 2;
                 int cy = (parentRl.getTop() + parentRl.getBottom()) / 2;
                 //起始大小半径
@@ -94,6 +96,49 @@ public class OpenScreenActivity extends Activity implements OpenScreen.onDialogL
         });
     }
 
+    private void endAnim() {
+        parentRl.post(new Runnable() {
+            @Override
+            public void run() {
+                int cx = (parentRl.getLeft() + parentRl.getRight()) / 2;
+                int cy = (parentRl.getTop() + parentRl.getBottom()) / 2;
+                float startR = 0f;
+                float endR = (float) Math.sqrt(cx * cx + cy * cy);
+                endAnim = ViewAnimationUtils.createCircularReveal(parentRl, cx, cy, endR, startR);
+                endAnim.setInterpolator(new AccelerateInterpolator());
+                endAnim.setDuration(600);
+                endAnim.addListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        finish();
+                        overridePendingTransition(0, 0);
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });
+                endAnim.start();
+            }
+        });
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        endAnim();
+    }
 
     class Timer extends CountDownTimer {
 
