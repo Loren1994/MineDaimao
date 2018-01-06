f5b16d1b47ddb4184e8716636481055bbdd85205
Loren <dayan805@163.com>
2018-01-06 17:45:59 +0800

    数字键盘

---
+++ app/src/main/java/com/example/loren/minesample/PwdInputActivity.kt
@@ -0,0 +1,30 @@
+package com.example.loren.minesample
+
+import android.view.View
+import com.example.loren.minesample.base.ui.BaseActivity
+import kotlinx.android.synthetic.main.pwd_input_activity.*
+import pers.victor.ext.toast
+
+/**
+ * Copyright © 05/01/2018 by loren
+ */
+class PwdInputActivity : BaseActivity() {
+    override fun initWidgets() {
+        key_board.onKBEvent = { str, type ->
+            when (type) {
+                key_board.KB_NUM -> input_edt.setText(str)
+                key_board.KB_CONFIRM -> toast("确认")
+                key_board.KB_CANCEL -> input_edt.setText(str)
+            }
+        }
+
+    }
+
+    override fun setListeners() {
+    }
+
+    override fun onWidgetsClick(v: View) {
+    }
+
+    override fun bindLayout() = R.layout.pwd_input_activity
+}
---
+++ app/src/main/java/com/example/loren/minesample/widget/KeyBoardView.kt
@@ -0,0 +1,55 @@
+package com.example.loren.minesample.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.example.loren.minesample.R
+import com.example.loren.minesample.annotation.AnimationClickType
+import com.example.loren.minesample.annotation.DynamicOnClickListener
+
+/**
+ * Copyright © 05/01/2018 by loren
+ */
+class KeyBoardView(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs), View.OnClickListener {
+
+    val KB_NUM = 0
+    val KB_CONFIRM = 1
+    val KB_CANCEL = 2
+    private var inputResult = StringBuilder()
+    var onKBEvent: ((str: String, type: Int) -> Unit)? = null
+    private val viewIds = arrayListOf(R.id.zero_tv, R.id.one_tv, R.id.two_tv, R.id.three_tv, R.id.four_tv, R.id.five_tv, R.id.six_tv, R.id.seven_tv, R.id.eight_tv, R.id.nine_tv)
+
+    init {
+        initView()
+    }
+
+    private fun initView() {
+        val view = View.inflate(context, R.layout.key_board_view, null)
+        setKBListener(view)
+        addView(view)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.confirm_ll -> onKBEvent?.invoke("", KB_CONFIRM)
+            R.id.cancel_ll -> {
+                inputResult.deleteCharAt(inputResult.lastIndex)
+                onKBEvent?.invoke(inputResult.toString(), KB_CANCEL)
+            }
+            else -> {
+                inputResult.append((v as TextView).text)
+                onKBEvent?.invoke(inputResult.toString(), KB_NUM)
+            }
+        }
+    }
+
+    private fun setKBListener(view: View) {
+        val listener = DynamicOnClickListener(this, AnimationClickType.ALPHA)
+        viewIds.forEachIndexed { _, id -> view.findViewById<TextView>(id).setOnClickListener(listener) }
+        view.findViewById<LinearLayout>(R.id.cancel_ll).setOnClickListener(listener)
+        view.findViewById<LinearLayout>(R.id.confirm_ll).setOnClickListener(listener)
+    }
+}
