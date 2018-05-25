c3344375359de26cbd28dcec9485b6e5b639a8c7
Loren <dayan805@163.com>
2018-05-25 17:17:17 +0800

    替换部分线程为协程

--- app/src/main/java/com/example/loren/minesample/FlagActivity.kt
+++ app/src/main/java/com/example/loren/minesample/FlagActivity.kt
@@ -1,11 +1,11 @@
 package com.example.loren.minesample
 
-import android.os.Bundle
-import android.os.SystemClock
-import android.support.v7.app.AppCompatActivity
 import android.view.View
+import com.example.loren.minesample.base.ext.log
 import com.example.loren.minesample.base.ui.BaseActivity
 import kotlinx.android.synthetic.main.flag_activity.*
+import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.android.UI
 import java.util.*
 
 /**
@@ -34,9 +34,37 @@ import java.util.*
  *                Copyright (c) 16-9-19 by loren
  */
 class FlagActivity : BaseActivity() {
+
+    private var randomArr = arrayOf(1000, 1500, 1800, 2000, 2200)
+    private var songArr = arrayOf("预备～", "唱！", "五星红旗迎风飘扬～", "胜利歌声多么响亮～", "歌唱我们亲爱的祖国～", "从今走向繁荣富强！", "不要停", "再来一遍！")
+    //    private lateinit var mThread: Thread
+    lateinit var cycle: Job
+
     override fun initWidgets() {
-        mThread = Thread { AutoCycleStr() }
-        mThread.start()
+//        mThread = Thread { AutoCycleStr() }
+//        mThread.start()
+        cycle = launch(UI) {
+            async {
+                autoCycleStr()
+            }.await()
+        }
+    }
+
+    private suspend fun autoCycleStr() {
+        while (true) {
+            log(">>>AutoCycleStr")
+            songArr.forEachIndexed { _, s ->
+                delay(randomArr[Random().nextInt(randomArr.size - 1)].toLong())
+                runOnUiThread { htv.animateText(s) }
+            }
+            yield()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+//        mThread.interrupt()
+        cycle.cancel()
     }
 
     override fun setListeners() {
@@ -45,23 +73,6 @@ class FlagActivity : BaseActivity() {
     override fun onWidgetsClick(v: View) {
     }
 
-    override fun bindLayout()=R.layout.flag_activity
+    override fun bindLayout() = R.layout.flag_activity
 
-    private lateinit var mThread: Thread
-    private var randomArr = arrayOf(1000, 1500, 1800, 2000, 2200)
-    private var songArr = arrayOf("预备～", "唱！", "五星红旗迎风飘扬～", "胜利歌声多么响亮～", "歌唱我们亲爱的祖国～", "从今走向繁荣富强！", "不要停", "再来一遍！")
-
-    private fun AutoCycleStr() {
-        while (true) {
-            songArr.forEachIndexed { _, s ->
-                SystemClock.sleep(randomArr[Random().nextInt(randomArr.size - 1)].toLong())
-                runOnUiThread { htv.animateText(s) }
-            }
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mThread.interrupt()
-    }
 }
