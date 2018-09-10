50e4c6c5ec94453d1099d4eb1d3b0692249ca531
loren1994 <dayan805@163.com>
2018-09-10 14:55:52 +0800

    openGLES05-三维

--- app/src/main/java/com/example/loren/minesample/opengl/FirstGlRender.kt
+++ app/src/main/java/com/example/loren/minesample/opengl/FirstGlRender.kt
@@ -18,7 +18,7 @@ import javax.microedition.khronos.opengles.GL10
 class FirstGlRender(val context: Context) : GLSurfaceView.Renderer {
 
     private var tableVerticlesWithTriangles = floatArrayOf(
-            //增加颜色属性x,y,r,g,b
+            //x,y,r,g,b
             0f, 0f, 1f, 1f, 1f,
             -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
             0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
@@ -40,9 +40,10 @@ class FirstGlRender(val context: Context) : GLSurfaceView.Renderer {
     private var U_MATRIX = "u_Matrix"
     private var uMatrixLocation = 0
     private var projectionMatrix = FloatArray(16)
+    private var modelMatrix = FloatArray(16)
     private var POSITION_COMPONENT_COUNT = 2
-    private var BYTE_PER_FLOAT = 4
     private var COLOR_COMPONENT_COUNT = 3
+    private var BYTE_PER_FLOAT = 4
     private var STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTE_PER_FLOAT
 
     init {
@@ -64,12 +65,13 @@ class FirstGlRender(val context: Context) : GLSurfaceView.Renderer {
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
         GLES20.glViewport(0, 0, width, height)
-        val aspectRatio = if (width > height) width.toFloat() / height.toFloat() else height.toFloat() / width.toFloat()
-        if (width > height) {
-            Matrix.orthoM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f, 1f)
-        } else {
-            Matrix.orthoM(projectionMatrix, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f, 1f)
-        }
+        Matrix.perspectiveM(projectionMatrix, 0, 45f, width.toFloat() / height.toFloat(), 1f, 10f)
+        Matrix.setIdentityM(modelMatrix, 0)
+        Matrix.translateM(modelMatrix, 0, 0f, 0f, -3f)
+        Matrix.rotateM(modelMatrix, 0, -60f, 1f, 0f, 0f)
+        val temp = FloatArray(16)
+        Matrix.multiplyMM(temp, 0, projectionMatrix, 0, modelMatrix, 0)
+        System.arraycopy(temp, 0, projectionMatrix, 0, temp.size)
     }
 
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
